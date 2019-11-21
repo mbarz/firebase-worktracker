9848b4c02f876e26130ab12a557823f61ecb9735
Max-Richard Barz <mbarz@muxe.de>
2019-11-21 14:42:48 +0100

    fixed pipe for negative durations

--- ui/src/app/duration.pipe.ts
+++ ui/src/app/duration.pipe.ts
@@ -5,11 +5,12 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DurationPipe implements PipeTransform {
   transform(value: any, ...args: any[]): any {
-    const minutes: number = value;
+    const negative = value < 0;
+    const minutes: number = Math.abs(value);
     const hours: number = Math.floor(minutes / 60);
-
-    return [hours, minutes - hours * 60]
+    const s = [hours, minutes - hours * 60]
       .map(n => n.toString().padStart(2, '0'))
       .join(':');
+    return negative ? `-${s}` : s;
   }
 }
